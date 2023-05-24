a1558b8ff60d7afd25383f2f1fdd40d6ae3987ac
Malek <100002726+m-Doss@users.noreply.github.com>
2023-05-24 12:59:50 -0500

    Update streamlit_app.py
    Fixed small bug

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,7 +27,10 @@ streamlit.dataframe(fruits_to_show)
 #displays fruityvice api response
 streamlit.header('Fruityvice Fruit Advice!')
 
-fruityvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # flattens the json object
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
